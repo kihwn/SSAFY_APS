c105805a6250c83f068773a5642d2d092d1caa07
kihwn <kihwn98@gmail.com>
2024-08-28 15:22:40 +0900

    feat:과제 완료

--- day15_0828수/src/병합정렬구현/Solution.java
+++ day15_0828수/src/병합정렬구현/Solution.java
@@ -1,5 +1,62 @@
 package 병합정렬구현;
 
+import java.util.Arrays;
+
 public class Solution {
+	static int[]arr = {2,91,20,21,13,9,38,11};
+	static int n = arr.length;
+	static int[]tmp = new int[n];
+	public static void main(String[]args) {
+		mergeSort(0,n-1);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	
+	static void mergeSort(int left, int right) {
+		if (left<right) {
+			int mid = (left+right)/2;
+			//가운데 기준으로 앞부분 정렬,
+			//뒷부분 정렬
+			//후 두 개 합치기
+			mergeSort(left,mid);
+			mergeSort(mid+1,right);
+			merge(left,mid,right);
+		}
+	}
+	
+	static void merge(int left, int mid, int right) {
+		int L = left; //왼쪽은 left~mid까지
+		int R = mid+1; //오른쪽은 mid+1~right까지
+		int idx = left; //tmp배열의 인덱스
+		
+		while(L<=mid && R<=right) {
+			if (arr[L]<=arr[R]) {
+				tmp[idx++]=arr[L++];
+			}else {
+				tmp[idx++]=arr[R++];
+			}
+		}
+		
+		//왼쪽이든 오른쪽이든 동시에 끝나지 않음. 무언가 남게 됨.
+		//왼쪽 구간의 값이 남은 경우
+		if (L<=mid) {
+			for (int i=L; i<=mid; i++) {
+				tmp[idx++]=arr[i];
+			}
+		}
+			
+			//오른쪽 구간의 값이 남은 경우.
+			else {
+				for (int i=R; i<=right; i++) {
+					tmp[idx++]=arr[i];
+				}
+				
+			}
+		
+		
+		for (int i=left; i<=right; i++) {
+			arr[i]=tmp[i];
+		}
+	}
 
 }
--- day15_0828수/src/퀵정렬구현/Solution.java
+++ day15_0828수/src/퀵정렬구현/Solution.java
@@ -1,5 +1,53 @@
 package 퀵정렬구현;
 
+import java.util.Arrays;
+
 public class Solution {
+	static int[]arr = {2,91,20,21,13,9,38,11};
+	static int n = arr.length;
+	public static void main(String[]args) {
+		quickSort(0,n-1);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	static void quickSort(int left, int right) {
+		//left : 시작 인덱스 , right : 마지막 인덱스
+		if (left<right) {
+			//배열을 피봇을 기준으로 두 부분으로 나누고, 피봇 리턴받음.
+			int pivot = partition(left,right);
+			quickSort(left,pivot-1);
+			quickSort(pivot+1,right);
+		}
+	}
+	
+	//피봇의 위치 반환
+	static int partition (int left, int right) {
+		//피봇 초기값
+		int pivot = arr[left];
+		//피봇이 left값이기 때문에 L은 그 다음 값부터 피봇보다 큰 값을 찾기 위한 값.
+		//R은 배열 끝부터 피봇보다 작은 값 찾기 위한 값
+		int L = left+1, R = right;
+		while(L<=R) {
+			while(L<=R && arr[L]<=pivot)
+				L++;
+			while(arr[R]>pivot)
+				R --;
+			
+			//L이 R보다 작거나 같은 경우까지는
+			//pivot보다 큰 L과 pivot보다 작은 R을 서로 뒤바꿔줌.
+			if (L<R) {
+				int tmp = arr[L];
+				arr[L] = arr[R];
+				arr[R]=tmp;
+			}
+		}
+		
+		//R의 위치는 피봇이 가야 할 위치. R의 위치가 피봇보다 작은 값과 큰 값의 경계가 되기 때문.
+		
+		int tmp = arr[left];
+		arr[left]=arr[R];
+		arr[R]=tmp;
+		return R;
+	}
 
 }
